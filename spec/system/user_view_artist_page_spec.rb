b77df83d5d3cce50ea4a95ab7b20d6a3ed8488ea
an-finocchiaro <andrea.amnaya@gmail.com>
2023-07-14 19:06:06 -0300

    user view artist page

--- spec/system/user_view_artist_page_spec.rb
+++ spec/system/user_view_artist_page_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'Usuário acessa página de artista' do
   it 'com sucesso' do
     artist = Artist.create!(name: 'Isabelle Tuchband')
-    canva = Canva.create!(artist_id: artist.id, code: 1108, collection_name: 'Brasileiras', title: 'Nuvens', technique: 'óleo sobre tela', height: 120, width: 100, year: 2011, frame:'sem moldura', price: 10000)
-    other_canva = Canva.create!(artist_id: artist.id, code: 1234, collection_name: 'Ciganas', title: 'Blue Gitane', technique: 'acrílica sobre tela', height: 100, width: 80, year: 2022, frame:'baguete', price: 8000)
+    canva = Canva.create!(artist_id: artist.id, code: 1108, collection_name: 'Brasileiras', title: 'Nuvens', technique: 'óleo sobre tela', height: 120, width: 100, year: 2011, frame:'sem moldura', price: 10000, display: :exhibit)
+    other_canva = Canva.create!(artist_id: artist.id, code: 1234, collection_name: 'Ciganas', title: 'Blue Gitane', technique: 'acrílica sobre tela', height: 100, width: 80, year: 2022, frame:'baguete', price: 8000, display: :exhibit)
     img_path = Rails.root.join('spec/support/images/canva_image.jpeg')
     canva.image.attach(io: img_path.open, filename: 'canva_image.jpeg')
     other_canva.image.attach(io: img_path.open, filename: 'canva_image.jpeg')
@@ -20,14 +20,13 @@ describe 'Usuário acessa página de artista' do
     expect(page).to have_content 'Nuvens'
     expect(page).to have_content '120 x 100 cm'
     expect(page).to have_content '2011'
-    expect(page).to have_content 'R$ 10000'
-    expect(page).to have_content 'código: 1108'
+    expect(page).to have_content 'referência: 1108'
     expect(page).to have_content 'Blue Gitane'
     expect(page).to have_content '100 x 80 cm'
     expect(page).to have_content '2022'
-    expect(page).to have_content 'código: 1234'
-    expect(page).to have_content 'R$ 8000'
+    expect(page).to have_content 'referência: 1234'
     expect(page).to have_css('img[src*="canva_image.jpeg"]')
     expect(page).to have_link 'BIOGRAFIA'
+    expect(page).to have_link 'SOLICITAR LISTA DE PREÇOS'
   end
 end
